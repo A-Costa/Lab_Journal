335f8e53e3b44558570988c4e222c8dc2fb46484
Alberto Costa <alberto.costa05@gmail.com>
2016-11-29 18:29:33 +0100

    Completate le funzioni di modifica (anche per i tag)

--- views.py
+++ views.py
@@ -20,6 +20,37 @@ def new():
     #in base ai valori presenti nel database
     nota_form.tags.choices = [(c.tag, c.tag) for c in Categoria.query.order_by(Categoria.tag).all()]
     if nota_form.validate_on_submit():
+        #categorie_db sono le categorie gia presenti nel database, categorie_ricevute le categorie ricevute sulla form
+        #categorie_nuove sono le categorie ricevute non presenti nel database (nuove appunto!)
+        categorie_db = [c.tag for c in Categoria.query.all()]
+        categorie_ricevute = nota_form.tags.data
+        categorie_nuove = set(categorie_ricevute)-set(categorie_db)
+        #inserimento delle categorie nuove nel db
+        for c in categorie_nuove:
+            new = Categoria(c)
+            db.session.add(new)
+            db.session.commit()
+        #creazione della nuova nota, con relativo append delle sue categorie
+        n = Nota(nota_form.titolo.data, datetime.date.today(), nota_form.testo.data)
+        for c in categorie_ricevute:
+            categoria = Categoria.query.filter(Categoria.tag == c).first()
+            n.categorie.append(categoria)
+        #aggiornamento del db
+        db.session.add(n)
+        db.session.commit()
+        return redirect('/nota/'+ str(n.id))
+    else:
+        return render_template('edit_note.html', nota_form = nota_form, errors = nota_form.errors)
+
+@app.route('/modify/<id_nota>', methods=['GET', 'POST'])
+def modify(id_nota):
+    n = Nota.query.filter(Nota.id == id_nota).first()
+    nota_form = Nota_Form()
+    nota_form.tags.choices = [(c.tag, c.tag) for c in Categoria.query.order_by(Categoria.tag).all()]
+
+    print nota_form.tags.default
+
+    if nota_form.validate_on_submit():
         categorie_db = [c.tag for c in Categoria.query.all()]
         categorie_ricevute = nota_form.tags.data
         categorie_nuove = set(categorie_ricevute)-set(categorie_db)
@@ -27,26 +58,19 @@ def new():
             new = Categoria(c)
             db.session.add(new)
             db.session.commit()
-        n = Nota(nota_form.titolo.data, datetime.date.today(), nota_form.testo.data)
+
+        n.titolo = nota_form.titolo.data
+        n.testo = nota_form.testo.data
+
         for c in categorie_ricevute:
             categoria = Categoria.query.filter(Categoria.tag == c).first()
             n.categorie.append(categoria)
-        db.session.add(n)
-        db.session.commit()
-        return redirect('/nota/'+ str(n.id))
-    else:
-        return render_template('new.html', nota_form = nota_form, errors = nota_form.errors)
 
-@app.route('/modify/<id_nota>', methods=['GET', 'POST'])
-def modify(id_nota):
-    nota_form = Nota_Form()
-    n = Nota.query.filter(Nota.id == id_nota).first()
-
-    if nota_form.validate_on_submit():
-        n.titolo = nota_form.titolo.data
-        n.testo = nota_form.testo.data
         db.session.commit()
         return redirect('/nota/'+str(n.id))
     else:
+        #le prossime due righe prendono dal database le informazioni gia presenti riguardo la nota che si sta modificando
+        nota_form.tags.data = [t.tag for t in n.categorie.all()]
         nota_form.testo.data = n.testo
-        return render_template('modify.html', nota_form=nota_form, errors=nota_form.errors, titolo=n.titolo)
+        nota_form.titolo.data = n.titolo
+        return render_template('edit_note.html', nota_form = nota_form, errors = nota_form.errors, modify=True)
